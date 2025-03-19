8b122487f49d1ae6160fc255253bae90e21fc9ea
perevedko <perevedko>
2025-03-19 17:28:17 +0400

    work in progress -- refactoring, splitting into different classes/modules

--- btc_wallet.rb
+++ btc_wallet.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 require 'bitcoin'
 require 'thor'
-require 'httparty'
 require 'json'
 require 'bigdecimal'
 require 'pry'
+require './mempool_space_api_client'
 
 Bitcoin.chain_params = :signet
 
 class WalletCLI < Thor
-  include HTTParty
-  base_uri 'https://mempool.space/signet/api'
-
   FEE = BigDecimal('0.00001')
   private_constant :FEE
 
@@ -28,7 +25,7 @@ class WalletCLI < Thor
   desc "balance", "Show wallet balance"
   def balance
     with_key do |key|
-      utxos = fetch_utxos(key.to_p2wpkh)
+      utxos = MempoolSpaceApiClient.utxos(key.to_p2wpkh)
       total = utxos.sum { |u| u['value'] }
       puts "Balance: #{satoshis_to_btc(total)} sBTC, address: #{key.to_p2wpkh}"
     end
@@ -41,14 +38,14 @@ class WalletCLI < Thor
       amount_sat = btc_to_satoshis(BigDecimal(amount))
       fee_sat = btc_to_satoshis(FEE)
 
-      utxos = fetch_utxos(key.to_p2wpkh)
+      utxos = MempoolSpaceApiClient.utxos(key.to_p2wpkh)
       total_input_sat = utxos.sum { |u| u['value'] }
       
       required_sat = amount_sat + fee_sat
       abort "Insufficient funds. Needed: #{satoshis_to_btc(required_sat)}" if total_input_sat < required_sat
 
       tx = build_transaction(key, utxos, address, amount_sat, fee_sat)
-      txid = broadcast_transaction(tx)
+      txid = MempoolSpaceApiClient.broadcast_transaction(tx)
       
       puts "Sent #{amount} sBTC to #{address}"
       puts "Transaction ID: #{txid}"
@@ -68,40 +65,16 @@ class WalletCLI < Thor
     yield Bitcoin::Key.from_wif(key_data)
   end
 
-  def fetch_utxos(address)
-    response = self.class.get("/address/#{address}/utxo")
-    if response.success?
-      JSON.parse(response.body)
-    else
-      abort "Failed to fetch UTXOs"
-    end
-  end
-
   def build_transaction(key, utxos, recipient, amount_sat, fee_sat)
     transaction = Bitcoin::Tx.new
-    details = utxos_details(utxos)
+    details = MempoolSpaceApiClient.utxos_details(utxos)
   
     add_inputs(transaction, details)
     add_outputs_and_change(key, transaction, details, recipient, amount_sat, fee_sat)
+    binding.pry
     sign(key, transaction, details)
   
     transaction
-  end
-
-  def utxos_details(utxos)
-    utxos.map do |utxo|
-      tx_response = self.class.get("/tx/#{utxo['txid']}")
-      abort "Failed to fetch TX #{utxo['txid']}" unless tx_response.success?
-      tx_data = JSON.parse(tx_response.body)
-      
-      vout = tx_data['vout'][utxo['vout']]
-      {
-        txid: utxo['txid'],
-        vout: utxo['vout'],
-        value: utxo['value'],
-        scriptpubkey: vout['scriptpubkey']
-      }
-    end
   end
 
   def add_inputs(transaction, utxos_details)
@@ -142,19 +115,8 @@ class WalletCLI < Thor
     end
   end
 
-  def broadcast_transaction(tx)
-    hex = tx.to_hex
-    response = self.class.post("/tx", body: hex)
-    
-    if response.success?
-      response.body
-    else
-      abort "Transaction failed: #{response.body}"
-    end
-  end
-
   def validate_address(addr)
-    abort "Invalid address format" unless Bitcoin::Script.parse_from_addr(addr)
+    Bitcoin::Script.parse_from_addr(addr)
   rescue
     abort "Invalid address format"
   end
---
+++ mempool_space_api_client.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+require 'httparty'
+
+class MempoolSpaceApiClient
+  include HTTParty
+  base_uri 'https://mempool.space/signet/api'
+
+  def self.utxos(address)
+    response = get("/address/#{address}/utxo")
+    if response.success?
+      JSON.parse(response.body)
+    else
+      abort "Failed to fetch UTXOs"
+    end
+  end
+
+  def self.utxos_details(utxos)
+    utxos.map do |utxo|
+      tx_response = get("/tx/#{utxo['txid']}")
+      abort "Failed to fetch TX #{utxo['txid']}" unless tx_response.success?
+      tx_data = JSON.parse(tx_response.body)
+
+      # TODO: double check it, it is correct?
+      vout = tx_data['vout'][utxo['vout']]
+      {
+        txid: utxo['txid'],
+        vout: utxo['vout'],
+        value: utxo['value'],
+        scriptpubkey: vout['scriptpubkey']
+      }
+    end
+  end
+
+  def self.broadcast_transaction(transaction)
+    hex = transaction.to_hex
+    response = self.class.post("/tx", body: hex)
+
+    if response.success?
+      response.body
+    else
+      abort "Transaction failed: #{response.body}"
+    end
+  end
+
+  def self.recommended_fees
+    response = get('/fees/recommended')
+    if response.success?
+      JSON.parse(response.body)
+    else
+      abort "Failed to fetch recommended fees"
+    end
+  end
+end
